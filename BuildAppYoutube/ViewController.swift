0be10a10d953bf1bb87fdbb4f1f28f7c6f665ff8
abczhijia <abczhijia@youzan.com>
2019-04-22 22:01:59 +0800

    1、BuildAppYoutube项目默认初始化

---
+++ BuildAppYoutube/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuildAppYoutube
+//
+//  Created by db on 2019/4/22.
+//  Copyright © 2019 db. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
