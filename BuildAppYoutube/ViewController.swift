5749ff1dc3010b8b8b1bcfc624ab48c1f8831606
abczhijia <abczhijia@youzan.com>
2019-04-23 09:39:57 +0800

    7、给VideoCell添加页面元素，使用pod加载第三方包：使用第三方包SnapKit来布局，使用第三方包Kingfisher来异步加载图片

--- BuildAppYoutube/ViewController.swift
+++ BuildAppYoutube/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import SnapKit
+import Kingfisher
+
 
 class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     let cellId = "cellId"
@@ -14,7 +17,7 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = UIColor.white
-        collectionView.register(ViewCell.self, forCellWithReuseIdentifier: cellId)
+        collectionView.register(VideoCell.self, forCellWithReuseIdentifier: cellId)
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -28,19 +31,102 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: view.frame.width, height: 200)
+        return CGSize(width: view.frame.width, height: 300)
     }
 }
 
 
-class ViewCell: UICollectionViewCell {
+class VideoCell: UICollectionViewCell {
+
+    static let videoCellMargin:CGFloat = 16
+    static let userProfileWidth:CGFloat = videoCellMargin * 3
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupViews()
     }
     
+    let thumbnailImageView:UIImageView = {
+        let iv = UIImageView()
+        iv.layer.masksToBounds = true
+        iv.backgroundColor = UIColor.blue
+        iv.contentMode = .scaleAspectFill
+        let url = URL(string: "http://pic150.nipic.com/file/20171224/8669400_090903351033_2.jpg")
+        iv.kf.setImage(with: url)
+        return iv
+    }()
+    
+    let seperatorView:UIView = {
+        let sepView = UIView()
+        sepView.backgroundColor = UIColor.gray
+        return sepView
+    }()
+    
+    let userProfileImageView:UIImageView = {
+        let iv = UIImageView()
+        iv.layer.masksToBounds = true
+        iv.layer.cornerRadius = VideoCell.userProfileWidth / 2
+        iv.backgroundColor = UIColor.red
+        iv.contentMode = .scaleAspectFill
+        
+        let url = URL(string: "http://tx.haiqq.com/uploads/allimg/170921/021505OS-8.jpg")
+        iv.kf.setImage(with: url)
+        return iv
+    }()
+    
+    let titleLabel: UILabel = {
+        let label = UILabel()
+        label.backgroundColor = UIColor.red
+        return label
+    }()
+    
+    let subtitleTextView: UITextView = {
+        let stv = UITextView()
+        stv.backgroundColor = UIColor.purple
+        return stv
+    }()
+    
     func setupViews() {
-        self.backgroundColor = UIColor.blue
+        addSubview(thumbnailImageView)
+        addSubview(seperatorView)
+        addSubview(userProfileImageView)
+        addSubview(titleLabel)
+        addSubview(subtitleTextView)
+        
+        thumbnailImageView.snp.makeConstraints { (m) in
+            m.top.equalTo(0)
+            m.left.equalTo(VideoCell.videoCellMargin)
+            m.right.equalTo(-VideoCell.videoCellMargin)
+            m.height.equalTo(200)
+        }
+        
+        seperatorView.snp.makeConstraints { (m) in
+            m.left.equalTo(0)
+            m.right.equalTo(0)
+            m.bottom.equalTo(0)
+            m.height.equalTo(1)
+        }
+        
+        userProfileImageView.snp.makeConstraints { (m) in
+            m.left.equalTo(VideoCell.videoCellMargin)
+            m.width.equalTo(VideoCell.userProfileWidth)
+            m.top.equalTo(thumbnailImageView.snp.bottom).offset(10)
+            m.height.equalTo(VideoCell.userProfileWidth)
+        }
+        
+        titleLabel.snp.makeConstraints { (m) in
+            m.left.equalTo(userProfileImageView.snp.right).offset(10)
+            m.right.equalTo(-VideoCell.videoCellMargin)
+            m.top.equalTo(thumbnailImageView.snp.bottom).offset(10)
+            m.height.equalTo(24)
+        }
+        
+        subtitleTextView.snp.makeConstraints { (m) in
+            m.left.equalTo(userProfileImageView.snp.right).offset(10)
+            m.right.equalTo(-VideoCell.videoCellMargin)
+            m.top.equalTo(titleLabel.snp.bottom).offset(10)
+            m.height.equalTo(24)
+        }
     }
     
     required init?(coder aDecoder: NSCoder) {
---
+++ Pods/Target Support Files/Pods-BuildAppYoutube/Pods-BuildAppYoutube-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BuildAppYoutube : NSObject
+@end
+@implementation PodsDummy_Pods_BuildAppYoutube
+@end
